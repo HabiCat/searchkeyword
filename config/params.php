120c4e73344bc9fc1dd6f135ddaf2cee03365bcd
habicat <habicator@gmail.com>
2015-08-14 14:27:00 +0800

    添加搜狗词典上传路径

--- config/params.php
+++ config/params.php
@@ -3,4 +3,5 @@ Yii::setAlias('@root', realpath(dirname(__FILE__).'/..'));
 return [
     'adminEmail' => 'admin@example.com',
     'salt' => 'abcdef',
+    'sougouDictDir' => ROOT_PATH . '/upload/dict/sougou',
 ];
