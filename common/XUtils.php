14996a3a221c4fd84a5513e842c254fad88a0c15
habicat <habicator@gmail.com>
2015-08-21 17:25:39 +0800

    增加敏感词过滤功能

--- common/XUtils.php
+++ common/XUtils.php
@@ -128,4 +128,59 @@ class XUtils {
 	    return $result['size'].$result['unit'];
 	}
 
+	/**
+	 * 字符串UTF8转换
+	 * @param  [type] $utf8_str [description]
+	 * @return [type]           [description]
+	 */
+	public static function get_chars($utf8_str){
+		$s = $utf8_str;
+		$len = strlen($s);
+		if($len == 0) return array();
+		$chars = array();
+		for($i = 0;$i < $len;$i++){
+			$c = $s[$i];
+			$n = ord($c);
+			if(($n >> 7) == 0){		//0xxx xxxx, asci, single
+				$chars[] = $c;
+			}
+			else if(($n >> 4) == 15){ 	//1111 xxxx, first in four char
+				if($i < $len - 3){
+					$chars[] = $c.$s[$i + 1].$s[$i + 2].$s[$i + 3];
+					$i += 3;
+				}
+			}
+			else if(($n >> 5) == 7){ 	//111x xxxx, first in three char
+				if($i < $len - 2){
+					$chars[] = $c.$s[$i + 1].$s[$i + 2];
+					$i += 2;
+				}
+			}
+			else if(($n >> 6) == 3){ 	//11xx xxxx, first in two char
+				if($i < $len - 1){
+					$chars[] = $c.$s[$i + 1];
+					$i++;
+				}
+			}
+		}
+		return $chars;
+	}
+
+	public static function loadcache($file) {
+		$ext = substr($file, strrpos($file, '.') + 1);
+		if($ext) {
+			switch ($ext) {
+				case 'tmp':
+					$content = file_get_contents($file);
+					break;
+				case 'php':
+					$content = include($file);
+					break;
+				default:
+					$content = false;
+			}
+		}
+
+		return $content;
+	}
 }
